0181a56fd9873eb35108ecb4fc2771b94fffe869
JavierPolaris <javierrojocanton@gmail.com>
2022-05-11 09:12:38 +0200

    Final formulario

---
+++ ejercicio.js
@@ -0,0 +1,330 @@
+document.getElementById('boton').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // a. Nom: e//Todos los nombres deben empezar por mayúsculas y el campo no admite números o símbolos, sólo letras. Se trata de un campo obligatorio.
+    let name = document.getElementById('name').value;
+    var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var cont = 0;
+    var carct = ['@', '!', '|', '#', '$', '%', '&', '/', '=', '?', '¿', '€', '¿']
+    if (name.charAt(0) != name.charAt(0).toUpperCase()) {
+        alert('En Nombre la primera letra ha de ser mayúscula');
+    } for (let i = 0; i < name.length; i++) {
+        if (name[i].charCodeAt() >= 65 && name[i].charCodeAt() <= 90) {
+            cont++;
+        }
+    }
+    var cont = 0;
+    for (let i = 0; i < num.length; i++) {
+        for (let j = 0; j < carct.length; j++) {
+            if (name.includes(num[i]) || name.includes(carct[j])) {
+                cont++;
+            }
+        }
+
+    }
+    if (cont == 0) {
+        console.log('ok');
+    } else {
+        alert('Nombre sólo puede contener letras')
+    }
+
+
+
+    // b. Apellidos: Todos los apellidos deben empezar por mayúsculas y kel campo no admite números o símbolos, sólo letras. Se trata de un campo obligatorio.
+
+    let apellido = document.getElementById('apellido').value;
+    var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var cont = 0;
+    var carct = ['@', '!', '|', '#', '$', '%', '&', '/', '=', '?', '¿', '€', '¿']
+    if (apellido.charAt(0) != apellido.charAt(0).toUpperCase()) {
+        alert('En Apellido la primera letra ha de ser mayúscula');
+    } for (let i = 0; i < apellido.length; i++) {
+        if (apellido[i].charCodeAt() >= 65 && apellido[i].charCodeAt() <= 90) {
+            cont++;
+        }
+    }
+    var cont = 0;
+    for (let i = 0; i < num.length; i++) {
+        for (let j = 0; j < carct.length; j++) {
+            if (apellido.includes(num[i]) || apellido.includes(carct[j])) {
+                cont++;
+            }
+        }
+
+    }
+    if (cont == 0) {
+        console.log('ok');
+    } else {
+        alert('Apellido sólo puede contener letras')
+    }
+
+
+
+
+
+    // c. ;Dirección: Formato: Tipo vía/ Nombre vía, Número, Resto de Datos (piso, portal...etc), CP, Población y País (separados por ,).Se trata de un campo obligatorio.
+    //el lo que ha hecho es meter en un array el formato de la dirección, cada elemento de la direccion es una posicion en el array
+    //y lo unico que compueba es que en el [1] haya una /
+    //el value que mete el usario se pasa a array tambien y se compara con el formato que tenemos preestablecido
+    //y si no hay comas simplemente lo entiende como una posicion y ya no coincide
+
+
+    // let direccion = document.getElementById('direccion').value;
+    // let arr = Array.from(direccion);
+
+    // let tipoVia = ['Calle/','Avenida/','Calleja/','Carretera/','Callejón/']
+    // let nombreVia = [] + ", ";
+    // let numVia = ['0, ','1, ','2, ','3, ','4, ','5, ','6, ','7, ','8, ','9, '];
+    // let restoVia = [] + ", ";
+    // let cpVia = [] + ", ";
+    // let poblacion = [] + ", ";
+    // let pais = [];
+    // let contc = 0;
+
+    // for(let i = 0; i < direccion.length; i++){
+    //     direccion[0] = tipoVia;
+    //     direccion[1] = nombreVia;
+    //     direccion[2] = numVia;
+    //     direccion[3] = restoVia;
+    //     direccion[4] = cpVia;
+    //     direccion[5] = poblacion;
+    //     direccion[6] = pais;
+
+    // }
+
+
+
+
+
+
+    // d. Email: Formato correcto: dirección(puede admitir puntos y guiones bajos)@nombreDominio.RestoDominio. Se trata de un campo obligatorio.
+    // El email se compone de tres partes: nombre usuario + @ + servidor + dominio.
+    // Nombre y servidor:
+    // 1. Debe de empezar por letra o numero. Al menos tiene una letra o       // /^\w+([\.-_]?\w+)*@\w+([\._]?\w+)*(\.\w{2,3})+$/.test(email))número. La letra o número se expresa mediante el carácter \w. que significa [A-Za-z0-9].
+
+    // Para asegurarnos de que la letra o número aparece al menos// una vez utilizaremos el modificador +.
+    // Puede contener puntos y guiones y guiones bajos además de las letras y números. Esta combinación podrá aparecer, es por ello que utilizaremos el modificador * (cero o varias veces). Insertaremos toda la combinación entre paréntesis.
+    // Su expresión regular, para ambos casos, será la siguiente: \w+([\.-]?\w+)*
+    /* 
+    2. Dominio :Irá al final, detrás de un punto. Podrá tener dos (.es, .fr, .it,…) o tres letras (.com, .net, .org,..) Si queremos indicar un número concreto de caracteres lo expresamos con el número entre los operadores { y }
+    Además podemos tener varios dominios seguidos (.com.ar, .com.uk,….) es por ello que deberemos de usar el modificador +. Ya que el dominio podrá aparecer varias veces.
+    Su expresión regular será comos sigue: */
+
+    // (\.\w{2,3,4})+  
+    // +	El carácter anterior puede aparecer 1 o más veces. 
+    /* ^	Ancla. Delimina el inicio del patrón. Significa empieza por.
+    $	Ancla. Delimina el final del patrón. Significa acaba en. */
+    //Caracter especial	Descripción []	Rango de carácteres. Cualquiera de los caracteres del interior de los corchetes.
+    //  .	Comodín, cualquier caracter.
+    // 	\ Invierte el significado de un carácter. Si es especial, lo escapa. Si no, lo vuelve especial.
+    // *	El carácter anterior puede aparecer 0 o más veces.
+
+    const email = document.getElementById('email').value;
+    if (/^\w+([\.-_]?\w+)*@\w+([\._]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        console.log("La dirección de email " + email + " es correcta.")
+    } else {
+        alert('La direccion de correo es incorrecta ');
+    }
+
+
+    // e. Confirmar email: Mismo formato y además debe coincidir con el rellenado anteriormente, va a servir de usuario en el login. Se trata de un campo obligatorio.
+
+
+    const checkEmail = document.getElementById('checkemail').value;
+
+    let loginCorrecto = document.getElementById('email').value;
+    if (checkEmail == loginCorrecto) {
+        console.log('El usuario es correcto y coincide con el login')
+    } else {
+        alert('El ususario es incorrecto no coincide el login, vuelve a intentarlo de nuevo')
+    }
+
+
+
+
+
+
+    // f. DNI: Debe ser un DNI válido. Se trata de un campo obligatorio.
+
+    //TODO Se puede utilizar un validador de html como el required en el que indico que es necesario
+    //TODO introducir 8 carcteres numericos y una letra acepta minusculas y mayusculas
+
+    //! pattern="[0-9]{8}[A-Za-z]{1}" title="Debe poner 8 números y una letra" 
+
+    let dni = document.getElementById('dni').value;
+    var espacios1 = false;
+    var cont1 = 0;
+    var mayus1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var minus1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    while (!espacios1 && (cont1 < dni.length)) {
+        if (dni.charAt(cont1) == " ")
+            espacios1 = true;
+        cont1++;
+
+
+        if (espacios1) {
+            alert("El DNI no puede contener espacios en blanco");
+            return false;
+        }
+        if (dni.length < 8 && dni.length > 9) {
+            alert("En DNI ha de introducir 8 caracteres numericos");
+            return false;
+        }
+        
+    };
+    var cont = 0;
+    for (let i = 0; i < mayus1.length; i++) {
+        if (dni.includes(mayus1[i])) {
+            cont++;
+        }
+
+    }
+    if (cont != 0 && cont < 2) {
+        console.log('ok')
+    } else {
+        alert('En DNI ha de introducir una letra Mayuscula')
+    }
+    
+        var cont = 0;
+    for (let i = 0; i < minus1.length; i++) {
+        if (dni.includes(minus1[i])) {
+            cont++;
+        }
+console.log(cont)
+    }
+    if (cont > 1) {
+        alert('En DNI no puede contener letras minusculas')
+    } else {
+        console.log('ok1')
+    }
+    
+
+
+    // g. Intereses: Uno de los tres válidos, dos o los tres; se deben separar por ",", si todos los incluídos son incorrectos se le deben volver a pedir y si hay alguno correcto, se incluyen los correctos.
+
+    const int = 'mercado inmobiliario';
+    const int1 = 'bolsa';
+    const int2 = 'bienes estatales';
+    const com = ',';
+
+    var letra = document.getElementById('interes').value;
+
+
+    if (letra == int + com + ' ' + int1) {
+        console.log('yes 1');
+
+    } else if (letra == int + com + ' ' + int2) {
+        console.log('yes 2');
+
+    } else if (letra == int1 + com + ' ' + int) {
+        console.log('yes 3');
+
+    } else if (letra == int1 + com + ' ' + int2) {
+        console.log('yes 4');
+
+    } else if (letra == int2 + com + ' ' + int) {
+        console.log('yes 5');
+
+    } else if (letra == int2 + com + ' ' + int1) {
+        console.log('yes 6');
+
+    } else if (letra == int || letra == int1 || letra == int2) {
+        console.log('yes');
+
+    } else {
+        alert(`En Intereses debes de introducir uno o todos ${int},${int1},${int2} y entre "," cada uno`);
+    }
+
+
+
+
+    // h. Password: Mínimo 8 caracteres y máximo 20, debe contener al menos una mayúscula,
+    // al menos una minúscula,
+    // al menos dos números y al menos un símbolo.
+    // Se trata de un campo obligatorio.
+
+
+    let pass = document.getElementById('pass').value;
+    var espacios = false;
+    var cont = 0;
+    var mayus = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var minus = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var carct = ['@', '!', '|', '#', '$', '%', '&', '/', '=', '?', '¿', '€', '¿']
+
+    while (!espacios && (cont < pass.length)) {
+        if (pass.charAt(cont) == " ")
+            espacios = true;
+        cont++;
+
+
+        if (espacios) {
+            alert("La contraseña no puede contener espacios en blanco");
+            return false;
+        }
+        if (pass.length < 8) {
+            alert("En Contraseña ha de introducir minimo 8 caracteres y maximo 20");
+            return false;
+        }
+        if (pass.length > 20) {
+            alert("En Contraseña ha de introducir minimo 8 caracteres y maximo 20");
+            return false;
+        };
+    };
+    
+    var cont = 0;
+    for (let i = 0; i < mayus.length; i++) {
+        if (pass.includes(mayus[i])) {
+            cont++;
+        }
+
+    }
+    if (cont != 0) {
+        console.log('ok')
+    } else {
+        alert('En Contraseña ha de introducir minimo una letra Mayuscula')
+    }
+    var cont = 0;
+    for (let i = 0; i < minus.length; i++) {
+        if (pass.includes(minus[i])) {
+            cont++;
+        }
+
+    }
+    if (cont != 0) {
+        console.log('ok1')
+    } else {
+        alert('En Contraseña ha de introducir minimo una letra Minusculas')
+    }
+
+    var cont = 0;
+    for (let i = 0; i < num.length; i++) {
+        if (pass.includes(num[i])) {
+            cont++;
+        }
+    }
+    if (cont >= 2) {
+        console.log('ok2')
+    } else {
+        alert('En Contraseña ha de introducir minimo 2 numeros diferentes')
+    }
+    var cont = 0;
+    for (let i = 0; i < carct.length; i++) {
+        if (pass.includes(carct[i])) {
+            cont++;
+        }
+    }
+    if (cont != 0) {
+        console.log('ok3')
+    } else {
+        alert('En Contraseña ha de introducir minimo 1 caracter')
+    }
+
+
+});
+
+
+
+
